ae44700a8f0fdcfb6440bba25a2f2662a7a4a5fe
Feri Nurjaman <lahferii>
2024-10-31 07:01:29 -0700

    add sizing for text loader

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,9 +1,11 @@
 export const Loader = ({message}) => {
   return(
-    <div className="absolute w-full h-full bg-white z-10 flex items-center justify-center duration-300">
-      <p className="text-lg tracking-widest font-semibold leading-relaxed animate-pulse">
-        {message}
-      </p>
+    <div className="absolute w-full h-full z-10 flex">
+      <div className="w-1/2 sm:w-full m-auto sm:w-full text-center">
+        <p className="text-lg tracking-widest font-semibold leading-relaxed animate-pulse">
+          {message}
+        </p>
+      </div>
     </div>
   )
 }
